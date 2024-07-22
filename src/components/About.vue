09cb955f294427bda41c5ae99df91adfcc32c290
fauziap <ujiahmadfauzi05@gmail.com>
2024-07-23 05:50:28 +0700

    perbaiki overflow dan svg

--- src/components/About.vue
+++ src/components/About.vue
@@ -8,16 +8,16 @@
                         alt="Profile Image" class="rounded-md shadow-teal-200 w-64 h-64">
                 </div>
                 <div data-aos="fade-left" data-aos-duration="1000" class="lg:w-3/5 md:mt-5 mt-8">
-                    <p class="text-lg mb-6">
+                    <p class="md:text-lg text-md mb-6">
                         I am a graduate of Network and Application Informatics System (SIJA) program, which is a 4-year equivalent to D1 degree. I have experience in developing web applications and responsive user interfaces.
                     </p>
-                    <p class="text-lg mb-6">
+                    <p class="md:text-lg text-md mb-6">
                         I have skills in HTML, CSS, PHP, and several frameworks. I also have knowledge of databases and working with MySQL. I enjoy learning new technologies and continuously expanding my expertise in software development.
                     </p>
-                    <p class="text-lg mb-6">
+                    <p class="md:text-lg text-md mb-6">
                         Furthermore, I possess analytical and problem-solving skills, as well as the ability to work in a team to achieve common goals. I have a particular interest in web-based application development.
                     </p>
-                    <p class="text-lg">
+                    <p class="md:text-lg text-md">
                         My goal as a programmer is to continually enhance my skills and knowledge in software development, and to contribute to the creation of innovative and useful technological solutions for users.
                     </p>
 
